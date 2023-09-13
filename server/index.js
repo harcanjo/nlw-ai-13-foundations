a250161f33f0a477598f5534cbc6ab3e28826120
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-13 10:39:18 -0300

    get video id

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import express from 'express'
 const app = express()
 app.use(cors())
 
-app.get('/summary', (request, response) => {
-  response.send("Hello World")
+app.get('/summary/:id', (request, response) => {
+  response.send("ID do vídeo: " + request.params.id)
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
