ac152169203c6dbbde44d0034133382c234d4b0c
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-14 16:44:21 -0300

    transcription returned from whisper

--- server/index.js
+++ server/index.js
@@ -11,17 +11,27 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/summary/:id', async (request, response) => {
-  await download(request.params.id)
-  const audioConverted = await convert()
-  console.log(audioConverted)
+  try {
+    await download(request.params.id)
+    const audioConverted = await convert()
+    console.log(audioConverted)
 
-  const result = await transcribe()
-  return response.json({ result })
+    const result = await transcribe(audioConverted)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error);
+    return response.json({ error })
+  }
 })
 
 app.post('/summary/', async (request, response) => {
-  const result = await summarize(request.body.text)
-  return response.json({ result })
+  try {
+    const result = await summarize(request.body.text)
+    return response.json({ result })
+  } catch (error) {
+    console.log(error);
+    return response.json({ error })
+  }
 })
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
