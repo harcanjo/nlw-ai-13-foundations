93097ca28e0f84a1873f4a7d55239cd1dbbae9b8
Hugo Arcanjo <hfarcanjo@gmail.com>
2023-09-14 14:20:59 -0300

    select videoID on form input

---
+++ web/form.js
@@ -0,0 +1,27 @@
+const form = document.querySelector("#form")
+const input = document.querySelector("#url")
+const content = document.querySelector("#content")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault()
+
+  const videoURL = input.value
+
+  if (!videoURL.includes("shorts")) {
+    return (content.textContent = "Esse vídeo não parece ser um short.")
+  }
+
+  const [_, params] = videoURL.split("/shorts/")
+  const [videoID] = params.split("?si")
+
+  content.textContent = "Obtendo o texto do áudio..."
+})
+
+
+
+
+
+
+
+
+
